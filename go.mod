9d2288e757d5ee6ba4a9639618003236e101ebb4
Wim <wim@42.be>
2021-11-12 01:06:05 +0100

    Update dependencies

--- go.mod
+++ go.mod
@@ -1,12 +1,23 @@
 module regtag
 
+go 1.17
+
+require github.com/containers/image/v5 v5.16.1
+
 require (
-	github.com/containers/image v0.0.0-20181220121929-0c6cc8e14200
-	github.com/docker/docker v1.13.1 // indirect
-	github.com/docker/docker-credential-helpers v0.6.1 // indirect
-	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
-	github.com/opencontainers/image-spec v1.0.1 // indirect
-	github.com/opencontainers/runc v0.1.1 // indirect
-	github.com/pkg/errors v0.8.0 // indirect
-	github.com/sirupsen/logrus v1.2.0 // indirect
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/containers/storage v1.37.0 // indirect
+	github.com/docker/docker-credential-helpers v0.6.4 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/moby/sys/mountinfo v0.5.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2-0.20210819154149-5ad6f50d6283 // indirect
+	github.com/opencontainers/runc v1.0.2 // indirect
+	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635 // indirect
+	golang.org/x/sys v0.0.0-20211111213525-f221eed1c01e // indirect
 )
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/containers/image/pkg/docker/config"
+	"github.com/containers/image/v5/pkg/docker/config"
 )
 
 const contentType = "application/vnd.docker.distribution.manifest.v2+json"
@@ -21,44 +21,58 @@ func getManifest(regURL, user, pass string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	req.Header.Set("Accept", contentType)
+
 	if user != "" {
 		req.Header.Set("Authorization", "Basic "+basicAuth(user, pass))
 	}
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
+
 	defer resp.Body.Close()
+
 	res, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
+
 	manifest := string(res)
 	if resp.StatusCode != 200 {
 		return "", fmt.Errorf("getting manifest failed: %s", manifest)
 	}
+
 	return manifest, nil
 }
 
 func addTag(regURL, user, pass, manifest string) error {
 	body := strings.NewReader(manifest)
+
 	req, err := http.NewRequest("PUT", regURL, body)
 	if err != nil {
 		return err
 	}
+
 	req.Header.Set("Content-Type", contentType)
+
 	if user != "" {
 		req.Header.Set("Authorization", "Basic "+basicAuth(user, pass))
 	}
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
+
 	defer resp.Body.Close()
+
 	if resp.StatusCode != 201 {
 		return fmt.Errorf("tagging failed, got status %v", resp.StatusCode)
 	}
+
 	return nil
 }
 
@@ -69,47 +83,61 @@ func basicAuth(username, password string) string {
 
 func main() {
 	var user, pass, creds string
+
 	flag.StringVar(&creds, "creds", "", "use [username[:password]] for accessing the registry")
 	flag.Parse()
+
 	if len(os.Args) == 1 {
 		fmt.Printf("Usage of %s:\n", os.Args[0])
 		fmt.Printf("%s registry/image:tag extratag (uses docker login credentials by default)", os.Args[0])
 		fmt.Println()
 		flag.PrintDefaults()
+
 		return
 	}
+
 	if creds != "" {
 		res := strings.Split(creds, ":")
 		user = res[0]
+
 		if len(res) > 1 {
 			pass = res[1]
 		}
 	}
+
 	if len(flag.Args()) != 2 {
 		fmt.Println("Usage:  regtag registry/image:tag extratag (uses docker login credentials by default)")
 		fmt.Println("\tregtag --creds [username[:password]] registry/image:tag extratag (if you need specific credentials)")
+
 		return
 	}
+
 	imageTag := flag.Arg(0)
 	newTag := flag.Arg(1)
+
 	if !strings.Contains(imageTag, "://") {
 		imageTag = "https://" + imageTag
 	}
+
 	myURL, err := url.Parse(imageTag)
 	if err != nil {
 		log.Fatalf("parsing failed: %s", err)
 	}
+
 	registry := myURL.Host
 	if registry == "" {
 		log.Fatalf("parsing failed, registry url not found in %s", imageTag)
 	}
+
 	res := strings.Split(myURL.Path, ":")
 	baseTag := "latest"
 	repo := ""
+
 	if len(res) == 2 {
 		repo = res[0]
 		baseTag = res[1]
 	}
+
 	if repo == "" {
 		log.Fatalf("parsing failed, registry url not found in %s", imageTag)
 	}
@@ -122,12 +150,15 @@ func main() {
 	}
 
 	baseURL := myURL.Scheme + "://" + registry + "/v2" + repo + "/manifests"
+
 	manifest, err := getManifest(baseURL+"/"+baseTag, user, pass)
 	if err != nil {
 		log.Fatalf("failed to get manifest on %s: %s", baseURL+"/"+baseTag, err)
 	}
+
 	if err = addTag(baseURL+"/"+newTag, user, pass, manifest); err != nil {
 		log.Fatalf("failed to set tag on %s: %s", baseURL+"/"+newTag, err)
 	}
+
 	fmt.Printf("%s added to %s\n", newTag, imageTag)
 }
